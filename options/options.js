2d149cdd0e3b97983b716ae266a27859c518e487
Crowdbotics <team@crowdbotics.com>
2023-02-16 19:50:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quiet-disk-38976",
+  url: "https://quiet-disk-38976.botics.co",
+  api: "https://quiet-disk-38976.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
